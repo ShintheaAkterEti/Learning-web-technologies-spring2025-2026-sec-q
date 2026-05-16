86465c1ba56389441c335a55589801f7d5034c1b
Shinthea Akter Eti <shintheaaktereti@gmail.com>
2026-05-16 08:17:15 +0600

    7th

---
+++ Tech/model/customerModel.php
@@ -0,0 +1,29 @@
+<?php
+require_once('../db.php');
+
+function registerCustomer($full_name, $username, $password){
+    $con = getConnection();
+
+    $full_name = mysqli_real_escape_string($con, $full_name);
+    $username  = mysqli_real_escape_string($con, $username);
+    $password  = mysqli_real_escape_string($con, $password);
+
+    // Check username already exists
+    $check = "SELECT * FROM customers WHERE username='$username'";
+    $result = mysqli_query($con, $check);
+
+    if($result && mysqli_num_rows($result) > 0){
+        return "Username already exists";
+    }
+
+    $sql = "INSERT INTO customers(full_name, username, password)
+            VALUES('$full_name', '$username', '$password')";
+
+    if(mysqli_query($con, $sql)){
+        return true;
+    }
+
+    return "Registration failed";
+}
+
+
